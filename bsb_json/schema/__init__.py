c9ef23bd120287a46dc46d91af324bb0e5f84eb4
Robin De Schepper <robin.deschepper93@gmail.com>
2023-11-08 13:23:46 +0100

    schemas

--- bsb_json/schema/__init__.py
+++ bsb_json/schema/__init__.py
@@ -4,24 +4,40 @@ from bsb.core import Scaffold
 from bsb.config import get_config_attributes
 
 
-def schema(root):
-    schema = object_schema(root)
-    schema["title"] = "Configuration"
-    schema["description"] = "Automated JSON schema of configuration object"
+def get_json_schema(root):
+    schema = get_schema(root)
     return json.dumps(schema)
 
-def object_schema(obj):
+
+def get_schema(root):
+    defs = {}
+    schema = object_schema(root, defs)
+    schema["title"] = "Configuration"
+    schema["description"] = "Automated JSON schema of configuration object"
+    schema["$defs"] = defs
+    return schema
+
+def object_schema(obj, defs=None):
     schema = {"type": "object", "properties": {}}
-    obj_hints = typing.get_type_hints(obj, localns={"Scaffold": Scaffold})
-    obj_attrs = get_config_attributes(obj)
+    cls = obj.__class__
+    obj_hints = typing.get_type_hints(cls, localns={"Scaffold": Scaffold})
+    obj_attrs = get_config_attributes(cls)
     for attr, descr in obj_attrs.items():
         hint = obj_hints.get(attr, str)
-        schema["properties"][attr] = attr_schema(hint)
+        schema["properties"][attr] = attr_schema(hint, defs)
 
     return schema
 
-def attr_schema(hint):
+def attr_schema(hint, defs=None):
+    if defs is None:
+        defs = {}
     schema = {}
+    try:
+        is_dict = issubclass(typing.get_origin(hint), dict)
+        is_list = issubclass(typing.get_origin(hint), list)
+    except TypeError:
+        is_dict = False
+        is_list = False
     if hint is str:
         schema["type"] = "string"
     elif hint is int:
@@ -30,20 +46,23 @@ def attr_schema(hint):
         schema["type"] = "number"
     elif hint is bool:
         schema["type"] = "boolean"
-    elif typing.get_origin(hint) is list:
+    elif is_list:
         schema["type"] = "array"
-        schema["items"] = attr_schema(typing.get_args(hint)[0])
-    elif typing.get_origin(hint) is dict:
+        schema["items"] = attr_schema(typing.get_args(hint)[0], defs)
+    elif is_dict:
         schema["type"] = "object"
         schema["properties"] = {}
-        schema["additionalProperties"] = attr_schema(typing.get_args(hint)[1])
+        schema["additionalProperties"] = attr_schema(typing.get_args(hint)[1], defs)
     else:
         try:
             is_node = get_config_attributes(hint)
         except:
             is_node = False
         if is_node:
-            schema = object_schema(hint)
+            key = defs_key(hint)
+            if key not in defs:
+                defs[key] = object_schema(hint)
+            return schema_def_ref(hint)
         else:
             schema["type"] = "object"
             schema["properties"] = {}
@@ -51,6 +70,8 @@ def attr_schema(hint):
 
     return schema
 
-from bsb.config import Configuration
+def defs_key(hint):
+    return str(hint.__name__)
 
-print(schema(Configuration))
+def schema_def_ref(hint):
+    return { "$ref": f"#/$defs/{defs_key(hint)}" }
